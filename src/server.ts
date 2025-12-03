75cb97be12bdbb44354627529a903e5343226e02
Limon Reza <limonreza3.1416@gmail.com>
2025-12-03 19:53:04 +0600

    add PUT and DELETE methon for Update and Delete

--- src/server.ts
+++ src/server.ts
@@ -91,6 +91,8 @@ try{
 
 })
 
+
+//Get all users
 app.get("/users" ,async (req:Request,res:Response)=>{
 try{
 
@@ -113,6 +115,8 @@ try{
 }
 })
 
+
+//Get single user by ID
 app.get("/users/:id", async (req:Request,res:Response)=>{
     // console.log(req.params.id)
     try{
@@ -132,13 +136,126 @@ app.get("/users/:id", async (req:Request,res:Response)=>{
 
         }
 
-
-
         console.log(result.rows)
+
     }catch(err:any){
 
     }
 })
+
+//Update user by ID
+app.put("/users/:id", async (req:Request,res:Response)=>{
+    // console.log(req.params.id);
+
+    const {name,email} = req.body;
+
+    try{
+
+        const result = await pool.query(`UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *`,
+            [name,email,req.params.id]
+        )
+
+        if (result.rows.length === 0){
+                res.status(404).json({
+                success : false,
+                message : "User not found "
+            })
+        } else{
+            res.status(201).json({
+                success : true,
+                message : "User Updated "
+            })
+        }
+
+    } catch(err:any){
+            res.status(500).json({
+                success : false,
+                message : "Server's Problem",
+                details : err.message
+            })
+    }
+})
+
+//Delete user by Id 
+app.delete("/users/:id", async (req:Request,res:Response)=>{
+    // console.log(req.params.id);
+
+    try{
+
+        const result = await pool.query(`DELETE FROM users WHERE id=$1`,
+            [req.params.id]
+        )
+
+        if (result.rowCount === 0){
+                res.status(404).json({
+                success : false,
+                message : "User not found "
+            })
+        } else{
+            res.status(201).json({
+                success : true,
+                message : "User Deleted ",
+                data : result.rows
+            })
+        }
+
+    } catch(err:any){
+            res.status(500).json({
+                success : false,
+                message : "Server's Problem",
+                details : err.message
+            })
+    }
+})
+
+
+//todo routes will be here
+app.post("/todos",async (req:Request,res:Response)=>{
+    const {user_id,title} = req.body;
+    try{
+        const result = await pool.query(`INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`,
+            [user_id,title]
+        )
+
+        res.status(201).json({
+            success:true,
+            message:"todos inserted successfully",
+            data : result.rows[0]
+        })
+    }catch(err:any){
+        res.status(500).json({
+            success:false,
+            message:"Something happening ",
+            details : err.message
+        })
+    }
+
+})
+
+app.get("/todos" ,async (req:Request,res:Response)=>{
+try{
+
+    const result = await pool.query(`SELECT * FROM todos`);
+    res.status(200).json({
+        success : true,
+        message : "We got All todos",
+        data : result.rows
+
+    })
+
+}catch(err:any){
+
+    res.status(500).json({
+        success:false,
+        message : err.message,
+        details :err
+    })
+
+}
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
